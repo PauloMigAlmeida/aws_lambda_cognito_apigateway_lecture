d7b73bd69122abdd58add5c824a6b49f90e865e7
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-11-23 10:18:58 -0200

    Add AP gateway generated SDK

---
+++ Application/FamilyMoments/FamilyMoments/generated-src/CLIFamilyMomentsClient.m
@@ -0,0 +1,176 @@
+/*
+ Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License").
+ You may not use this file except in compliance with the License.
+ A copy of the License is located at
+
+ http://aws.amazon.com/apache2.0
+
+ or in the "license" file accompanying this file. This file is distributed
+ on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied. See the License for the specific language governing
+ permissions and limitations under the License.
+ */
+ 
+
+#import "CLIFamilyMomentsClient.h"
+#import <AWSCore/AWSCore.h>
+#import <AWSCore/AWSSignature.h>
+#import <AWSCore/AWSSynchronizedMutableDictionary.h>
+
+#import "CLIListMomentsResponse.h"
+#import "CLIDefaultMomentResponse.h"
+#import "CLICreateMomentRequest.h"
+
+@interface AWSAPIGatewayClient()
+
+// Networking
+@property (nonatomic, strong) NSURLSession *session;
+
+// For requests
+@property (nonatomic, strong) NSURL *baseURL;
+
+// For responses
+@property (nonatomic, strong) NSDictionary *HTTPHeaderFields;
+@property (nonatomic, assign) NSInteger HTTPStatusCode;
+
+- (AWSTask *)invokeHTTPRequest:(NSString *)HTTPMethod
+                     URLString:(NSString *)URLString
+                pathParameters:(NSDictionary *)pathParameters
+               queryParameters:(NSDictionary *)queryParameters
+              headerParameters:(NSDictionary *)headerParameters
+                          body:(id)body
+                 responseClass:(Class)responseClass;
+
+@end
+
+@interface CLIFamilyMomentsClient()
+
+@property (nonatomic, strong) AWSServiceConfiguration *configuration;
+
+@end
+
+@interface AWSServiceConfiguration()
+
+@property (nonatomic, strong) AWSEndpoint *endpoint;
+
+@end
+
+@implementation CLIFamilyMomentsClient
+
+@synthesize configuration = _configuration;
+
+static AWSSynchronizedMutableDictionary *_serviceClients = nil;
+
++ (instancetype)defaultClient {
+    AWSServiceConfiguration *serviceConfiguration = nil;
+    if ([AWSServiceManager defaultServiceManager].defaultServiceConfiguration) {
+        serviceConfiguration = AWSServiceManager.defaultServiceManager.defaultServiceConfiguration;
+    } else {
+        serviceConfiguration = [[AWSServiceConfiguration alloc] initWithRegion:AWSRegionUnknown
+                                                           credentialsProvider:nil];
+    }
+
+    static CLIFamilyMomentsClient *_defaultClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _defaultClient = [[CLIFamilyMomentsClient alloc] initWithConfiguration:serviceConfiguration];
+    });
+
+    return _defaultClient;
+}
+
++ (void)registerClientWithConfiguration:(AWSServiceConfiguration *)configuration forKey:(NSString *)key {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _serviceClients = [AWSSynchronizedMutableDictionary new];
+    });
+    [_serviceClients setObject:[[CLIFamilyMomentsClient alloc] initWithConfiguration:configuration]
+                        forKey:key];
+}
+
++ (instancetype)clientForKey:(NSString *)key {
+    return [_serviceClients objectForKey:key];
+}
+
++ (void)removeClientForKey:(NSString *)key {
+    [_serviceClients removeObjectForKey:key];
+}
+
+- (instancetype)init {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:@"`- init` is not a valid initializer. Use `+ defaultClient` or `+ clientForKey:` instead."
+                                 userInfo:nil];
+    return nil;
+}
+
+- (instancetype)initWithConfiguration:(AWSServiceConfiguration *)configuration {
+    if (self = [super init]) {
+        _configuration = [configuration copy];
+
+        NSString *URLString = @"https://ujnf7mmh0h.execute-api.us-east-1.amazonaws.com/live";
+        if ([URLString hasSuffix:@"/"]) {
+            URLString = [URLString substringToIndex:[URLString length] - 1];
+        }
+        _configuration.endpoint = [[AWSEndpoint alloc] initWithRegion:_configuration.regionType
+                                                              service:AWSServiceAPIGateway
+                                                                  URL:[NSURL URLWithString:URLString]];
+
+        AWSSignatureV4Signer *signer = [AWSSignatureV4Signer signerWithCredentialsProvider:_configuration.credentialsProvider
+                                                                                  endpoint:_configuration.endpoint];
+
+        _configuration.baseURL = _configuration.endpoint.URL;
+        _configuration.requestInterceptors = @[[AWSNetworkingRequestInterceptor new], signer];
+    }
+    
+    return self;
+}
+
+- (AWSTask *)momentsGet {
+    NSDictionary *headerParameters = @{
+                                       @"Content-Type": @"application/json",
+                                       @"Accept": @"application/json",
+                                       
+                                       };
+    NSDictionary *queryParameters = @{
+                                      
+                                      };
+    NSDictionary *pathParameters = @{
+                                     
+                                     };
+    
+    return [self invokeHTTPRequest:@"GET"
+                         URLString:@"/moments"
+                    pathParameters:pathParameters
+                   queryParameters:queryParameters
+                  headerParameters:headerParameters
+                              body:nil
+                     responseClass:[CLIListMomentsResponse class]];
+}
+
+- (AWSTask *)momentsPost:(CLICreateMomentRequest *)body {
+    NSDictionary *headerParameters = @{
+                                       @"Content-Type": @"application/json",
+                                       @"Accept": @"application/json",
+                                       
+                                       };
+    NSDictionary *queryParameters = @{
+                                      
+                                      };
+    NSDictionary *pathParameters = @{
+                                     
+                                     };
+    
+    return [self invokeHTTPRequest:@"POST"
+                         URLString:@"/moments"
+                    pathParameters:pathParameters
+                   queryParameters:queryParameters
+                  headerParameters:headerParameters
+                              body:body
+                     responseClass:[CLIDefaultMomentResponse class]];
+}
+
+
+
+@end
